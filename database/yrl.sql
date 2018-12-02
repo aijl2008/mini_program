bf96785c7b841f79364a69c12dd630fb48f551b9
aijl2008 <Jerry790928>
2018-12-02 12:37:11 +0800

    commit from mac

--- database/yrl.sql
+++ database/yrl.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.24, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.21, for osx10.13 (x86_64)
 --
 -- Host: localhost    Database: yrl
 -- ------------------------------------------------------
--- Server version	5.7.24
+-- Server version	5.7.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -56,7 +56,7 @@ CREATE TABLE `followed_user` (
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,7 +65,6 @@ CREATE TABLE `followed_user` (
 
 LOCK TABLES `followed_user` WRITE;
 /*!40000 ALTER TABLE `followed_user` DISABLE KEYS */;
-INSERT INTO `followed_user` VALUES (1,'aaaaaaaaaaaaaaaaaaaaaaaaaaaaa','ccccccccccccccccccccccccccccc','2018-11-23 14:11:59','2018-11-23 14:11:59'),(3,'aaaaaaaaaaaaaaaaaaaaaaaaaaaaa','bbbbbbbbbbbbbbbbbbbbbbbbbbbb','2018-11-23 14:33:18','2018-11-23 14:33:18');
 /*!40000 ALTER TABLE `followed_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -109,7 +108,7 @@ CREATE TABLE `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -118,7 +117,6 @@ CREATE TABLE `migrations` (
 
 LOCK TABLES `migrations` WRITE;
 /*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
-INSERT INTO `migrations` VALUES (1,'2014_10_12_000000_create_users_table',1),(2,'2014_10_12_100000_create_password_resets_table',1),(3,'2016_06_01_000001_create_oauth_auth_codes_table',1),(4,'2016_06_01_000002_create_oauth_access_tokens_table',1),(5,'2016_06_01_000003_create_oauth_refresh_tokens_table',1),(6,'2016_06_01_000004_create_oauth_clients_table',1),(7,'2016_06_01_000005_create_oauth_personal_access_clients_table',1),(8,'2018_11_17_091844_create_wechats_table',1),(9,'2018_11_17_104717_create_failed_jobs_table',1),(10,'2018_11_17_114514_videos',1),(11,'2018_11_17_114556_create_videos_table',1),(12,'2018_11_17_114628_create_watching_table',1),(13,'2018_11_17_114642_create_watched_table',1);
 /*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -302,7 +300,7 @@ CREATE TABLE `user_video` (
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -311,7 +309,6 @@ CREATE TABLE `user_video` (
 
 LOCK TABLES `user_video` WRITE;
 /*!40000 ALTER TABLE `user_video` DISABLE KEYS */;
-INSERT INTO `user_video` VALUES (1,'aaaaaaaaaaaaaaaaaaaaaaaaaaaaa',2,'2018-11-23 12:40:36','2018-11-23 12:40:36'),(2,'aaaaaaaaaaaaaaaaaaaaaaaaaaaaa',2,'2018-11-23 12:41:03','2018-11-23 12:41:03'),(3,'aaaaaaaaaaaaaaaaaaaaaaaaaaaaa',21,'2018-11-23 12:41:08','2018-11-23 12:41:08'),(4,'aaaaaaaaaaaaaaaaaaaaaaaaaaaaa',1,'2018-11-23 12:41:15','2018-11-23 12:41:15'),(5,'aaaaaaaaaaaaaaaaaaaaaaaaaaaaa',7,'2018-11-23 13:55:49','2018-11-23 13:55:49'),(6,'aaaaaaaaaaaaaaaaaaaaaaaaaaaaa',7,'2018-11-23 13:57:07','2018-11-23 13:57:07'),(8,'aaaaaaaaaaaaaaaaaaaaaaaaaaaaa',5,'2018-11-23 14:36:59','2018-11-23 14:36:59');
 /*!40000 ALTER TABLE `user_video` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -342,7 +339,6 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('0','Jerry','10000000000','awz@awz.cn','\'\'\'\'\'\'','不支持密码登录',NULL,'2018-11-23 04:19:57','2018-11-23 04:19:57'),('3','Jerry','10000000000','awz@awz.cn','\'\'\'\'\'\'','不支持密码登录',NULL,'2018-11-23 06:59:53','2018-11-23 06:59:53'),('4','Jerry','10000000000','awz@awz.cn','\'\'\'\'\'\'','不支持密码登录',NULL,'2018-11-23 07:00:01','2018-11-23 07:00:01'),('aaaaaaaaaaaaaaaaaaaaaaaaaaaaa','Jerry','10000000000','awz@awz.cn','/upload/images/Nvazp75X3afXymmoGthO4eX5pF4kTIeyp94ahYBg.jpeg','不支持密码登录',NULL,'2018-11-23 04:20:30','2018-11-24 07:27:14'),('bbbbbbbbbbbbbbbbbbbbbbbbbbbb','Jerry','10000000000','awz@awz.cn','\'\'\'\'\'\'','不支持密码登录',NULL,'2018-11-23 05:20:29','2018-11-23 05:20:29'),('ccccccccccccccccccccccccccccc','Jerry','10000000000','awz@awz.cn','\'\'\'\'\'\'','不支持密码登录',NULL,'2018-11-23 05:20:40','2018-11-23 05:20:40'),('dddddddddddddddddddddddddddd','Jerry','10000000000','awz@awz.cn','\'\'\'\'\'\'','不支持密码登录',NULL,'2018-11-23 05:20:52','2018-11-23 05:20:52');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -370,7 +366,7 @@ CREATE TABLE `videos` (
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -379,8 +375,43 @@ CREATE TABLE `videos` (
 
 LOCK TABLES `videos` WRITE;
 /*!40000 ALTER TABLE `videos` DISABLE KEYS */;
-INSERT INTO `videos` VALUES (1,'The HongKong-Zhuhai-Macao Bridge is open!','5285890783150096757','','','2018-11-22 12:11:58','1',0,0,0,0,1,1,'2018-11-22 12:11:58','2018-11-22 12:11:58'),(2,'The HongKong-Zhuhai-Macao Bridge is open!','5285890783150096757','','','2018-11-22 12:12:11','1',0,0,0,0,1,1,'2018-11-22 12:12:11','2018-11-22 12:12:11'),(3,'The HongKong-Zhuhai-Macao Bridge is open!','5285890783150096757','','','2018-11-22 12:12:13','1',0,0,0,0,1,1,'2018-11-22 12:12:13','2018-11-22 12:12:13'),(4,'The HongKong-Zhuhai-Macao Bridge is open!','5285890783150096757','','','2018-11-22 12:12:15','1',0,0,0,0,1,1,'2018-11-22 12:12:15','2018-11-22 12:12:15'),(5,'The HongKong-Zhuhai-Macao Bridge is open!','5285890783150096757','','','2018-11-22 12:12:17','1',0,0,0,0,1,1,'2018-11-22 12:12:17','2018-11-22 12:12:17'),(6,'The HongKong-Zhuhai-Macao Bridge is open!','5285890783150096758','','','2018-11-23 11:51:42','1',0,0,0,0,1,1,'2018-11-23 11:51:42','2018-11-23 11:51:42'),(7,'The HongKong-Zhuhai-Macao Bridge is open!','5285890783150096759','','','2018-11-23 11:51:49','1',0,0,0,0,1,1,'2018-11-23 11:51:49','2018-11-23 11:51:49'),(8,'The HongKong-Zhuhai-Macao Bridge is open!','5285890783150096760','','','2018-11-23 11:51:56','1',0,0,0,0,1,1,'2018-11-23 11:51:56','2018-11-23 11:51:56'),(9,'The HongKong-Zhuhai-Macao Bridge is open!','5285890783150096760','','','2018-11-23 14:53:55','aaaaaaaaaaaaaaaaaaaaaaaaaaaaa',0,0,0,0,1,1,'2018-11-23 14:53:55','2018-11-23 14:53:55'),(10,'大轰炸.mp4','http://1258021496.vod2.myqcloud.com/1461fbc8vodcq1258021496/b96d18985285890783192961475/TK5eJ2aIcBQA.mp4','','','2018-11-24 07:07:36','aaaaaaaaaaaaaaaaaaaaaaaaaaaaa',0,0,0,0,1,1,'2018-11-24 07:07:36','2018-11-24 07:07:36'),(11,'浪漫的爱心美食.mp4','http://1258021496.vod2.myqcloud.com/1461fbc8vodcq1258021496/f3afbcca5285890783193158021/TkabkYs2LEcA.mp4','','','2018-11-24 07:34:07','aaaaaaaaaaaaaaaaaaaaaaaaaaaaa',0,0,0,0,1,1,'2018-11-24 07:34:07','2018-11-24 07:34:07'),(12,'The HongKong-Zhuhai-Macao Bridge is open!','52858907831500967601','https://www.jb51.net/images/logo.gif','11111','2018-11-24 15:37:44','aaaaaaaaaaaaaaaaaaaaaaaaaaaaa',0,0,0,0,1,1,'2018-11-24 15:37:44','2018-11-24 15:37:44'),(13,'The HongKong-Zhuhai-Macao Bridge is open!','1111','https://www.jb51.net/images/logo.gif','11111','2018-11-24 15:37:57','aaaaaaaaaaaaaaaaaaaaaaaaaaaaa',0,0,0,0,1,1,'2018-11-24 15:37:57','2018-11-24 15:37:57'),(14,'The HongKong-Zhuhai-Macao Bridge is open!','11111','https://www.jb51.net/images/logo.gif','11111','2018-11-24 15:38:01','aaaaaaaaaaaaaaaaaaaaaaaaaaaaa',0,0,0,0,1,1,'2018-11-24 15:38:01','2018-11-24 15:38:01'),(15,'The HongKong-Zhuhai-Macao Bridge is open!','111111','https://www.jb51.net/images/logo.gif','11111','2018-11-24 15:38:03','aaaaaaaaaaaaaaaaaaaaaaaaaaaaa',0,0,0,0,1,1,'2018-11-24 15:38:03','2018-11-24 15:38:03'),(16,'The HongKong-Zhuhai-Macao Bridge is open!','11111111','https://www.jb51.net/images/logo.gif','11111','2018-11-24 15:38:06','aaaaaaaaaaaaaaaaaaaaaaaaaaaaa',0,0,0,0,1,1,'2018-11-24 15:38:06','2018-11-24 15:38:06'),(17,'The HongKong-Zhuhai-Macao Bridge is open!','111111111','https://www.jb51.net/images/logo.gif','11111','2018-11-24 15:38:08','aaaaaaaaaaaaaaaaaaaaaaaaaaaaa',0,0,0,0,1,1,'2018-11-24 15:38:08','2018-11-24 15:38:08'),(18,'The HongKong-Zhuhai-Macao Bridge is open!','1111111111','https://www.jb51.net/images/logo.gif','11111','2018-11-24 15:38:11','aaaaaaaaaaaaaaaaaaaaaaaaaaaaa',0,0,0,0,1,1,'2018-11-24 15:38:11','2018-11-24 15:38:11'),(19,'The HongKong-Zhuhai-Macao Bridge is open!','11111111111','https://www.jb51.net/images/logo.gif','11111','2018-11-24 15:38:13','aaaaaaaaaaaaaaaaaaaaaaaaaaaaa',0,0,0,0,1,1,'2018-11-24 15:38:13','2018-11-24 15:38:13'),(20,'The HongKong-Zhuhai-Macao Bridge is open!','111111111111','https://www.jb51.net/images/logo.gif','11111','2018-11-24 15:38:16','aaaaaaaaaaaaaaaaaaaaaaaaaaaaa',0,0,0,0,1,1,'2018-11-24 15:38:16','2018-11-24 15:38:16'),(21,'The HongKong-Zhuhai-Macao Bridge is open!','1111111111111','https://www.jb51.net/images/logo.gif','11111','2018-11-24 15:38:18','aaaaaaaaaaaaaaaaaaaaaaaaaaaaa',0,0,0,0,1,1,'2018-11-24 15:38:18','2018-11-24 15:38:18');
 /*!40000 ALTER TABLE `videos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `wechats`
+--
+
+DROP TABLE IF EXISTS `wechats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `wechats` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `open_id` char(47) DEFAULT NULL,
+  `union_id` char(36) DEFAULT NULL,
+  `avatar` varchar(300) NOT NULL DEFAULT '',
+  `nickname` varchar(45) NOT NULL DEFAULT '',
+  `sex` tinyint(4) NOT NULL DEFAULT '0',
+  `country` varchar(45) NOT NULL DEFAULT '',
+  `province` varchar(45) NOT NULL DEFAULT '',
+  `city` varchar(45) NOT NULL DEFAULT '',
+  `mobile` char(11) NOT NULL DEFAULT '',
+  `status` tinyint(4) NOT NULL DEFAULT '1',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `open_id_UNIQUE` (`open_id`),
+  UNIQUE KEY `union_id_UNIQUE` (`union_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `wechats`
+--
+
+LOCK TABLES `wechats` WRITE;
+/*!40000 ALTER TABLE `wechats` DISABLE KEYS */;
+/*!40000 ALTER TABLE `wechats` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -392,4 +423,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-25  0:43:48
+-- Dump completed on 2018-12-02 12:34:12
