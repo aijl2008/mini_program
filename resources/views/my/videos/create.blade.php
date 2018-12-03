fec9735af5457f49598ba30f65e6e648cb78de93
aijl2008 <Jerry790928>
2018-12-03 20:00:47 +0800

    完成视频上传

--- resources/views/my/videos/create.blade.php
+++ resources/views/my/videos/create.blade.php
@@ -3,87 +3,75 @@
 @section('content')
     <div id="page-content" class="single-page">
         <div class="container">
-            <div class="row">
-                <div id="main-content" class="col-md-8">
-
-                    <form class="form-horizontal" id="form">
-                        <div class="form-group">
-                            <label for="inputPassword" class="col-sm-2 control-label">请输入视频名称</label>
-                            <div class="col-sm-10">
-                                <input type="text" name="title" id="title" required="">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="inputPassword" class="col-sm-2 control-label">谁可以看</label>
-                            <div class="col-sm-10">
-                                <div class="radio">
-                                    <label>
-                                        <input type="radio" name="visibility" id="optionsRadios1" value="1" checked>
-                                        任何人都可以看
-                                    </label>
-                                </div>
-                                <div class="radio">
-                                    <label>
-                                        <input type="radio" name="visibility" id="optionsRadios2" value="2">
-                                        关注我的人可以看
-                                    </label>
-                                </div>
-                                <div class="radio">
-                                    <label>
-                                        <input type="radio" name="visibility" id="optionsRadios3" value="3">
-                                        只有我自己可以看
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-sm-2 control-label">上传进度</label>
-                            <div class="col-sm-10">
-                                <p class="form-control-static" id="process"></p>
-                            </div>
-                        </div>
-
-                        <div class="form-group hidden">
-                            <label class="col-sm-2 control-label">video</label>
-                            <div class="col-sm-10">
-                                <input type="file" name="video" id="video" class="form-control">
-                            </div>
-                        </div>
-
-                        <div class="form-group hidden">
-                            <label class="col-sm-2 control-label">URL</label>
-                            <div class="col-sm-10">
-                                <input type="text" name="url" id="url" class="form-control">
-                            </div>
-                        </div>
-
-                        <div class="form-group hidden">
-                            <label class="col-sm-2 control-label">file_id</label>
-                            <div class="col-sm-10">
-                                <input type="text" name="file_id" id="file_id" class="form-control">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-sm-offset-2 col-sm-10">
-                                <button id="selectFile" type="button" class="btn" style="background-color:#fd0005">
-                                    选择视频文件
-                                </button>
-                                <button id="uploadFile" type="button" class="btn btn-primary">开始上传</button>
-                            </div>
-                        </div>
-
-                    </form>
+            <form id="form">
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">选择视频</label>
+                    <div class="col-sm-10">
+                        <input type="text" value="" name="url" id="url">
+                        <input type="text" value="" name="file_id" id="file_id">
+                        <p class="form-control-static" id="queue_videos"><a id="addVideo"
+                                                                            href="javascript:void(0);"
+                                                                            class="btn btn-outline">添加视频</a></p>
+                    </div>
                 </div>
-            </div>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">选择视频封面</label>
+                    <div class="col-sm-10">
+                        <input type="text" value="" name="cover_url" id="cover_url">
+                        <p class="form-control-static" id="queue_video_covers"><a id="addCover"
+                                                                                  href="javascript:void(0);"
+                                                                                  class="btn btn-outline">添加封面</a>
+                        </p>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword" class="col-sm-2 control-label">请输入视频名称</label>
+                    <div class="col-sm-10">
+                        <input type="text" name="title" id="title" required="">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword" class="col-sm-2 control-label">谁可以看</label>
+                    <div class="col-sm-10">
+                        <div class="radio">
+                            <label>
+                                <input type="radio" name="visibility" id="optionsRadios1" value="1" checked>
+                                任何人都可以看
+                            </label>
+                        </div>
+                        <div class="radio">
+                            <label>
+                                <input type="radio" name="visibility" id="optionsRadios2" value="2">
+                                关注我的人可以看
+                            </label>
+                        </div>
+                        <div class="radio">
+                            <label>
+                                <input type="radio" name="visibility" id="optionsRadios3" value="3">
+                                只有我自己可以看
+                            </label>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <a id="uploadFile" href="javascript:void(0);" class="btn btn-outline">上传并保存</a>
+                        </div>
+                    </div>
+                </div>
+            </form>
+            <input id="addVideo-file" type="file" style="display:block;"/>
+            <input id="addCover-file" type="file" style="display:block;"/>
         </div>
     </div>
 @endsection
 @section('js')
-
     <script src="//imgcache.qq.com/open/qcloud/js/vod/sdk/ugcUploader.js"></script>
     <script type="text/javascript">
+
         $(function () {
-            var file;
+            var index = 0;
+            var cosBox = [];
             /**
              * 计算签名
              **/
@@ -106,68 +94,121 @@
                 });
             };
 
-
-            var startUploader = function (file) {
-                var resultMsg = qcVideo.ugcUploader.start({
-                    videoFile: file,
-                    getSignature: getSignature,
-                    allowAudio: 0,
-                    success: function (result) {
-                    },
-                    error: function (result) {
-                        alert("上传失败" + result.msg);
-                    },
-                    progress: function (result) {
-                        console.log(result);
-                        $("#process").html(Math.floor(result.shacurr * 100) + '%');
-                    },
-                    finish: function (result) {
-                        $("#url").val(result.videoUrl);
-                        $("#file_id").val(result.fileId);
-                        $.ajax({
-                            url: '{{route("api.my.videos.store")}}',
-                            type: 'POST',
-                            data: $("#form").serialize(),
-                            dataType: 'json',
-                            success: function (res) {
-                                if (res && res.code == 0){
-                                    alert("上传成功");
-                                    window.location.href = "{{route('my.videos.index')}}";
-                                }
-                                else{
-                                    if (res.msg){
-                                        alert("保存视频失败,"+res.msg);
-                                    }
-                                    else{
-                                        alert("保存视频失败");
-                                    }
-                                }
-                            },
-                            error: function (res, err) {
-                                alert('发布视频失败:' + err);
-                            }
-                        });
-                    }
+            /*
+             * 取消上传绑定事件，示例一与示例二通用
+             */
+            $('#resultBox').on('click', '[act=cancel-upload]', function () {
+                var cancelresult = qcVideo.ugcUploader.cancel({
+                    cos: cosBox[$(this).attr('cosnum')],
+                    taskId: $(this).attr('taskId')
                 });
+                console.log(cancelresult);
+            });
+
+
+            var videoFileList = [];
+            var coverFileList = [];
+            // 给addVideo添加监听事件
+            $('#addVideo-file').on('change', function (e) {
+                var videoFile = this.files[0];
+                videoFileList[0] = videoFile;
+                $('#queue_videos').html(videoFile.name);
+                $("#title").val(videoFile.name);
+
+            });
+            $('#addVideo').on('click', function () {
+                $('#addVideo-file').click();
+            });
+            // 给addCover添加监听事件
+            $('#addCover-file').on('change', function (e) {
+                var coverFile = this.files[0];
+                coverFileList[0] = coverFile;
+                $('#queue_video_covers').html(coverFile.name);
+
+            });
+            $('#addCover').on('click', function () {
+                $('#addCover-file').click();
+            });
+
+            var startUploader = function () {
+                if (videoFileList.length) {
+                    //var num = addUploaderMsgBox();
+
+                    var resultMsg = qcVideo.ugcUploader.start({
+                        videoFile: videoFileList[0],
+                        coverFile: coverFileList[0],
+                        getSignature: getSignature,
+                        allowAudio: 1,
+                        success: function (result) {
+                            console.log('success', result);
+                            if (result.type == 'video') {
+                                $('#queue_videos').html('上传成功');
+                            } else if (result.type == 'cover') {
+                                $('#queue_video_covers').html('上传成功');
+                            }
+                        },
+                        error: function (result) {
+                            console.log('error', result);
+                            if (result.type == 'video') {
+                                $('#queue_videos').html(result.msg);
+                            } else if (result.type == 'cover') {
+                                $('#queue_video_covers').html(result.msg);
+                            }
+                        },
+                        progress: function (result) {
+                            if (result.type == 'video') {
+                                $('#queue_videos').html(Math.floor(result.curr * 100) + '%');
+                            } else if (result.type == 'cover') {
+                                $('#queue_video_covers').html(Math.floor(result.curr * 100) + '%');
+                            }
+                        },
+                        finish: function (result) {
+                            if (result.fileId) {
+                                $('#file_id').val(result.fileId);
+                            }
+                            if (result.coverUrl) {
+                                $('#cover_url').val(result.coverUrl);
+                            }
+                            $('#url').val(result.videoUrl);
+                            $.ajax({
+                                url: '{{route("api.my.videos.store")}}',
+                                type: 'POST',
+                                data: $("#form").serialize(),
+                                dataType: 'json',
+                                success: function (res) {
+                                    if (res && res.code == 0) {
+                                        alert("上传成功");
+                                        window.location.href = "{{route('my.videos.index')}}";
+                                    }
+                                    else {
+                                        if (res.msg) {
+                                            alert("保存视频失败," + res.msg);
+                                        }
+                                        else {
+                                            alert("保存视频失败");
+                                        }
+                                    }
+                                },
+                                error: function (res, err) {
+                                    alert('发布视频失败:' + err);
+                                }
+                            });
+                        }
+                    });
+
+                } else {
+                    alert('请添加视频！\n');
+                }
+
             };
 
+            // 上传按钮点击事件
             $('#uploadFile').on('click', function () {
-                var secretId = $('#secretId').val();
-                var secretKey = $('#secretKey').val();
-                startUploader(file);
+                startUploader();
+                $('#form').reset();
             });
-
-            $('#video').on('change', function (e, a, b) {
-                file = this.files[0];
-                $("#title").val(file.name);
-                $("#process").html("请点击上传按钮");
-            });
-            $('#selectFile').on('click', function () {
-                $('#video').click();
-            });
-        })
+        });
     </script>
-
     @endsection
     </body>
     </html>
--- resources/views/my/videos/index.blade.php
+++ resources/views/my/videos/index.blade.php
@@ -4,25 +4,7 @@
 @section('content')
     <div id="page-content" class="index-page">
         <div class="container">
-            <div class="row">
-                <div class="featured">
-                    <div class="main-vid">
-                        @foreach([] as $row)
-                            <div class="col-md-6">
-                                <div class="zoom-container">
-                                    <div class="zoom-caption">
-                                        <span>click_number{{$row->click_number}}</span>
-                                        <a href="{{ route("my.videos.show", $row->id) }}">
-                                            <i class="fa fa-play-circle-o fa-5x" style="color: #fff"></i>
-                                        </a>
-                                        <p>{{$row->title}}</p>
-                                    </div>
-                                    <img src="/images/1.jpg"/>
-                                </div>
-                            </div>
-                        @endforeach
-                    </div>
-                </div>
+            <div class="row video_list">
             </div>
         </div>
     </div>
@@ -31,7 +13,9 @@
 @section('js')
 
     <script type="text/javascript">
-        function ls(page = 1) {
+        var page = 1;
+
+        function ls() {
             $.ajax(
                 {
                     url: '/api/my/videos?page=' + page,
@@ -50,13 +34,22 @@
                             template += '</a>';
                             template += '<p>' + item.title + '</p>';
                             template += '</div>';
-                            template += '<img style="width:854px; height:487px" src="' + item.cover_url + '"/>';
+                            if (item.cover_url) {
+                                template += '<img src="' + item.cover_url + '"/>';
+                            }
+                            else {
+                                template += '<img src="/images/default_cover.jpg" />';
+                            }
                             template += '</div>';
+                            template += '<h3 class="vid-name"><a href="#">Video\'s Name</a></h3>';
                             template += '</div>';
                             videos.push(template);
                         });
-                        console.log(videos.join());
-                        $('.main-vid').append(videos.join());
+                        console.log(videos.length);
+                        if (videos.length > 0) {
+                            $('.video_list').append(videos.join(""));
+                        }
+
                     },
                     error: function (res, err, msg) {
                         if (res.status == 401) {
@@ -68,8 +61,7 @@
             );
         }
 
-        var page = 1;
-        ls(page);
+        ls();
         $(window).scroll(function () {
             if ($(window).scrollTop() + $(window).height() + 1 >= $(document).height()) {
                 page++;
